9c378d2036665a0b3d4b2ac7b78e639d9977c433
hupantingxue <hupantingxue@126.com>
2014-03-08 22:15:01 +0800

    client full code;
    A complete client;

--- network/tcp/client.cpp
+++ network/tcp/client.cpp
@@ -1,14 +1,33 @@
 #include "tcp_header.h"
 
 int main(int argc, char **argv) {
-	int sockfd = 0;
+    int sockfd = 0;
+    struct sockaddr_in servaddr,cliaddr;
+    char sendline[1000];
+    char recvline[1000];
 
     if (3 > argc) {
 	    printf("Usage: bin ip port\n");
 		return 0;
 	}
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	
+    bzero(&servaddr,sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr=inet_addr(argv[1]);
+    servaddr.sin_port=htons(32000);
+
+    connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+    while (fgets(sendline, 10000, stdin) != NULL)
+    {
+        sendto(sockfd,sendline,strlen(sendline),0,
+             (struct sockaddr *)&servaddr,sizeof(servaddr));
+        n=recvfrom(sockfd,recvline,10000,0,NULL,NULL);
+        recvline[n]=0;
+        fputs(recvline,stdout);
+    }
 
     return 0;
 }
