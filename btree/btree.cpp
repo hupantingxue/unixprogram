82f36ff6d804d5dafa25c37e075543813f6bf1ce
wuliang <wuliang@devc64.(none)>
2014-10-27 17:49:36 +0800

    Init btree sample code;

---
+++ btree/btree.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "btree.h"
+
+int main(int argc, char *argv[]) {
+    printf("Binary Tree Test Sample\n");
+}
---
+++ btree/btree.h
@@ -0,0 +1,16 @@
+/**********************************************************
+ * Author: hupantingxue
+ * Date:   2014-10-27
+ * Desc:
+ *         Binary Tree Sample;
+ *********************************************************/
+#ifndef _BTREE_H
+#define _BTREE_H
+
+typedef struct _BTNode {
+    int data;
+    struct _BTNode *left;
+    struct _BTNode *right;
+} *BTNode;
+
+#endif
